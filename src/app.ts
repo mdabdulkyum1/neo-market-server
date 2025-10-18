0dde6057cf20aaee9d40cc5c41e5eebb0afb4686
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-10-18 18:20:21 +0600

    feat: add morgan and http-status middleware; add app.ts and server.ts successfully run my server

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import cors from "cors";
+import express, { Application, Request, Response, NextFunction } from "express";
+import httpStatus from "http-status";
+import morgan from "morgan";
+
+
+const app: Application = express();
+
+const authOptions = {
+   origin: true,
+   credentials: true,
+   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+ }
+
+app.use(
+  cors(authOptions)
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({ Message: "Neo Market server is running. . ." });
+});
+
+app.use(morgan("dev"));
+
+app.use((req: Request, res: Response) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,38 @@
-console.log("hi there, hello")
+import { Server } from "http";
+import app from "./app";
+
+const port = process.env.PORT || 5000;
+
+async function main() {
+  const server: Server = app.listen(port, () => {
+    console.log("🚀 Neo Server is running on port", port);
+  });
+
+  // Graceful shutdown
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.log("🛑 Server closed");
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  };
+
+  const unexpectedErrorHandler = (error: unknown) => {
+    console.error("💥 Unexpected error:", error);
+    exitHandler();
+  };
+
+  process.on("uncaughtException", unexpectedErrorHandler);
+  process.on("unhandledRejection", unexpectedErrorHandler);
+  process.on("SIGTERM", () => {
+    console.log("🔄 SIGTERM received");
+    if (server) {
+      server.close();
+    }
+  });
+}
+
+main();
