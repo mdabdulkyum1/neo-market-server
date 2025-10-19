4ce0a89990eee71ae91e6ee5fdb130b98a60d202
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-10-19 21:23:37 +0600

    modified routes and add all routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
-
-const AuthRouters = "hello"
+import { AuthRouters } from "../modules/auth/auth.routes";
+import { UserRouters } from "../modules/user/Users.route";
+import { ReferralRouters } from "../modules/referral/referral.routes";
+import { PurchaseRouters } from "../modules/purchase/purchase.routes";
+import { EmailRouters } from "../modules/email/email.routes";
 
 const router = express.Router();
 
@@ -8,9 +11,24 @@ const moduleRoutes = [
     {
         path:"/auth",
         route: AuthRouters,
+    },
+    {
+        path:"/users",
+        route: UserRouters,
+    },
+    {
+        path:"/referrals",
+        route: ReferralRouters,
+    },
+    {
+        path:"/purchases",
+        route: PurchaseRouters,
+    },
+    {
+        path:"/email",
+        route: EmailRouters,
     }
 ]
-
 
 moduleRoutes.forEach(route => router.use(route.path, route.route))
 
